902902a610b3b21d6735816f5603a785e40e2005
S34nGS <steenhuis20@gmail.com>
2026-01-29 17:11:25 +0100

    to mac

--- week 1/2.1/t06/Program.cs
+++ week 1/2.1/t06/Program.cs
@@ -3,5 +3,29 @@
     static void Main()
     {
         List<string> myList = [];
+
+        Console.WriteLine($"Amount of tasks: {myList.Count}");
+        foreach (var item in myList)
+        {
+            Console.WriteLine(item);
+        }
+
+        myList.Add("Mow lawn");
+        myList.Add("Pay taxes");
+
+        Console.WriteLine($"Amount of tasks: {myList.Count}");
+        foreach (var item in myList)
+        {
+            Console.WriteLine(item);
+        }
+
+        myList.Remove("Mow lawn");
+        myList.Add("Shopping");
+
+        Console.WriteLine($"Amount of tasks: {myList.Count}");
+        foreach (var item in myList)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
---
+++ week 1/2.1/t07/Program.cs
@@ -0,0 +1,29 @@
+﻿static class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> fruitBasket = new() {
+            { "Apple", 3 },
+            { "Orange", 2 },
+            { "Pear", 4 },
+        };
+
+        Console.Write("Add what fruit? ");
+        string fruitToAdd = Console.ReadLine()!;
+
+        if (fruitBasket.ContainsKey(fruitToAdd))
+        {
+            fruitBasket[fruitToAdd] += 1;
+        }
+        else
+        {
+            fruitBasket.TryAdd(fruitToAdd, 1);
+        }
+
+        Console.WriteLine("\nFruit count:");
+        foreach (string fruit in fruitBasket.Keys)
+        {
+            Console.WriteLine($" - {fruit}: {fruitBasket[fruit]}");
+        }
+    }
+}
